c8fee009712f1489f5eb758b036fc6c912eddb5b
masc23 <email@ma-sc.eu>
2019-11-07 16:34:16 +0100

    First experiments with a composite structure

--- chunked-fs-cli/Program.cs
+++ chunked-fs-cli/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.IO;
+using chunked_fs;
 
 namespace chunked_fs_cli
 {
-	class Program
+	internal static class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			const string storageDir = @"C:\WOW\ChunkedFS\test-storage";
+			const string inputFile = @"C:\WOW\ChunkedFS\chunked-fs\README.md";
+			const string virtualFile = "/testfile.txt";
+
+			IVfs vfs = new SimpleVfs(storageDir);
+			Console.WriteLine(vfs.AddFile(virtualFile, File.OpenRead(inputFile)) ? "success" : "error");
 		}
 	}
 }
